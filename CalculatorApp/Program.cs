e96cc95dc4aaff99da1a3a402db97bfdcdc6d673
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-22 22:54:50 +0000

    refactored code

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -20,11 +20,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseCors("ReactDev");
 
 app.UseHttpsRedirection();
 
