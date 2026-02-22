677294da2951dc1eea83d3c71d5f76f8a3a3d9f0
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-22 16:18:13 +0000

    Add project files.

---
+++ CalculatorApp/Controllers/CalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to the Calculator API! Use POST /api/calculator with a JSON body to perform calculations.");
+        }
+
+    }
+}
